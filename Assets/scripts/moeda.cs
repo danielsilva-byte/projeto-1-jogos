36c47c1b7679e48d395d6cb1d14422a606ccb521
amanddot <amanda8mart@gmail.com>
2026-09-08 15:54:40 -0300

    moeda

---
+++ Assets/scripts/moeda.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+public class TriggerExemplo : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+                }
+    }
+
+    
+    }
+
