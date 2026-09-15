7284bfe59e7a33b84b31c909ebf266c35cd2cb37
giihrapozo <giihrapozinha13@gmail.com>
2026-09-15 15:34:37 -0300

    contador

--- Assets/scripts/moeda.cs
+++ Assets/scripts/moeda.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class TriggerExemplo : MonoBehaviour
 {
+
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            other.gameObject.SendMessage("ChangeTextCoin");
             Destroy(gameObject);
                 }
     }
